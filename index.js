ca162897c43bb3df1a01a0832286cb205faf4528
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2019-02-07 12:17:47 -0500

    ohoihoihoi

--- index.js
+++ index.js
@@ -4,26 +4,81 @@ const states = {
 	rejected: 'Rejected'
 };
 
+class Proxy {
+	constructor(value) {
+		this.value = value;
+		const trailingInvocations = [];
+		this.addTrailingInvocation = (cachedCallback, cachedHandler) => trailingInvocations
+			.push([cachedCallback, cachedHandler]);
+		this.runTrailingInvocations = () => {
+			for (const [cachedCallback, cachedHandler] of trailingInvocations) {
+				cachedCallback(cachedHandler(value));
+			}
+		}
+	}
+
+	then(onFulfilled) {
+		return new Nancy(resolve => this.addTrailingInvocation(resolve, onFulfilled));
+	}
+
+	catch(onRejected) {
+		return new Nancy(resolve => this.addTrailingInvocation(resolve, onRejected));
+	}
+}
+
+class ResolvedProxy extends Proxy {
+	constructor(value) {
+		super(value);
+		this.state = states.resolved;
+	}
+
+	then(onFulfilled) {
+		return Nancy.resolve(onFulfilled(this.value));
+	}
+
+	catch(onRejected) {
+		return this;
+	}
+}
+
+class RejectedProxy extends ResolvedProxy {
+	constructor(value) {
+		super(value);
+		this.state = states.rejected;
+	}
+
+	then(onFulfilled) {
+		return this;
+	}
+
+	catch(onRejected) {
+		return Nancy.resolve(onRejected(this.value));
+	}
+}
+
 class Nancy {
 	constructor(executor) {
-		this.state = states.pending;
-		const trailingInvocations = {
-			[states.resolved]: [],
-			[states.rejected]: []
+		this.proxy = {
+			state: states.pending
 		};
-		this.addTrailingInvocation = (state, cachedCallback, cachedHandler) => trailingInvocations[state]
-			.push([cachedCallback, cachedHandler]);
 		const getCallback = state => value => {
-			this.value = value;
-			this.state = state;
-			for (const [cachedCallback, cachedHandler] of trailingInvocations[state]) {
-				const invoke = () => cachedCallback(cachedHandler(this.value));
-				if (this.value instanceof Nancy) {
-					this.value.then(invoke);
-				} else {
-					invoke();
-				}
+			if (value instanceof Nancy) {
+				debugger
+				value.then(value => {
+					this.proxy = new ResolvedProxy(value);
+				});
+				value.catch(value => {
+					this.proxy = new RejectedProxy(value);
+				});
+			} else if (state === states.resolved) {
+				this.proxy = new ResolvedProxy(value);				
+			} else if (state === states.rejected) {
+				this.proxy = new RejectedProxy(value);
+			} else {
+				this.proxy = new Proxy(value);
+				this.proxy.state = state;
 			}
+			this.proxy.runTrailingInvocations();
 		};
 
 		try {
@@ -31,6 +86,14 @@ class Nancy {
 		} catch (error) {
 			getCallback(states.rejected)(error);
 		}
+	}
+
+	get state() {
+		return this.proxy.state;
+	}
+
+	get value() {
+		return this.proxy.value;
 	}
 
 	static resolve(value) {
@@ -41,20 +104,12 @@ class Nancy {
 		return new Nancy((resolve, reject) => reject(value));
 	}
 
-	then(onFulfilled) {
-		if (this.state === states.resolved) {
-			return new Nancy(resolve => resolve(onFulfilled(this.value)));
-		}
-
-		return new Nancy(resolve => this.addTrailingInvocation(states.resolved, resolve, onFulfilled));
+	then(onFulfilled = value => {}) {
+		return this.proxy.then(onFulfilled);
 	}
 
-	catch(onRejected) {
-		if (this.state === states.rejected) {
-			return new Nancy(resolve => resolve(onRejected(this.value)));
-		}
-
-		return new Nancy(resolve => this.addTrailingInvocation(states.rejected, resolve, onRejected));
+	catch(onRejected = value => {}) {
+		return this.proxy.catch(onRejected);
 	}
 }
 
